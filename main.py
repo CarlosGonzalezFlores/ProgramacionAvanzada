eaf014424cd411ba65736addec83d919937cc04f
Carlos González Flores <gonzalezflorescarlos394@gmail.com>
2024-09-09 17:54:12 -0600

    Tarea4/Unidad2

---
+++ main.py
@@ -0,0 +1,10 @@
+from coche import Coche
+
+coche_uno = Coche("BMW", "Coupé", 2020)
+coche_uno.mostrar_info()
+
+coche_dos = Coche("Volkswagen", "Taigun", 2023)
+coche_dos.mostrar_info()
+
+coche_tres = Coche("Nissan", "Tsuru", 2004)
+coche_tres.mostrar_info()
